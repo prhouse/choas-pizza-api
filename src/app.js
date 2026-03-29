7e29d2b4c70ce88dcab48adfbb806fd741221f1f
Morgan Ratgris <morgan.ratgris@etu.univ-grenoble-alpes.fr>
2026-03-29 11:55:22 +0200

    security express

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const routes = require('./routes/routes');
 
 const app = express();
 
+app.disable('x-powered-by');
+
 app.use(express.json());
 app.use('/', routes);
 app.use(express.static('public'));
